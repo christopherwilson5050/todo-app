06c2b6bb16722b0e6a0fe63afc60bedd158d6771
chris <christopherwilson5050@gmail.com>
2025-04-07 20:35:21 +0100

    Added the ability to add a task to do which is written to a txt file

--- Python 30 days to-do list/gui.py
+++ Python 30 days to-do list/gui.py
@@ -2,9 +2,22 @@ from functions import *
 import FreeSimpleGUI as fsg
 
 lable = fsg.Text("Type in tasks to-do: ")
-input_box = fsg.InputText(tooltip="Enter a task to do")
+input_box = fsg.InputText(tooltip="Enter a task to do", key="addtask")
 add_button = fsg.Button("Add")
 
-inputWindow = fsg.Window('My To-Do App', layout=[[lable, input_box],[add_button]] )
-inputWindow.read()
+inputWindow = fsg.Window('My To-Do App',
+                         layout=[[lable, input_box,add_button]],
+                         font=('Arial', 12) )
+while True:
+    event, values = inputWindow.read()
+    match event:
+        case "Add":
+            todos = get_todos()
+            addtodo = values['addtask'] + '\n'
+            todos.append(addtodo)
+            write_todos(todos)
+        case fsg.WIN_CLOSED:
+            break
+
+
 inputWindow.close()
